da50fb4b501fae41115e9f93f3df17c18fb6c333
netosilveira <me@netosilveira.dev>
2021-11-16 20:51:18 -0300

    Carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,84 @@
 <template>
-<div>
-<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
-  <img class="w3-image" src="../assets/img/world.jpg" alt="World map" width="1500" height="800">
-</header>
-</div>
+  <zd-card
+    name="container"
+    component="ZdContainer"
+    cssClass="zd-ma-2"
+    cssStyle="height:calc(100vh - 80px)"
+    v-bind="{
+      name: 'card',
+      component: 'ZdCard',
+      height: '100%',
+      children: [
+        {
+          name: 'navigationCarousel',
+          component: 'ZdCarousel',
+          prevButton: {
+            name: 'prevButtonNavigation',
+            component: 'ZdButton',
+            iconName: 'arrow-left',
+            icon: 'true',
+            large: true,
+          },
+          nextButton: {
+            name: 'nextButtonNavigation',
+            component: 'ZdButton',
+            iconName: 'arrow-right',
+            icon: 'true',
+            large: true,
+          },
+          children: [
+            {
+              name: 'slideNavigation1',
+              component: 'ZdRow',
+              justify: 'center',
+              children: [
+                {
+                  name: 'slideCardNavigation1',
+                  component: 'ZdCard',
+                  width: '800',
+                  height: '450',
+                  img: 'https://picsum.photos/id/350/800/400',
+                },
+              ],
+            },
+            {
+              name: 'slideNavigation2',
+              component: 'ZdRow',
+              justify: 'center',
+              children: [
+                {
+                  name: 'slideCardNavigation2',
+                  component: 'ZdCard',
+                  width: '800',
+                  height: '450',
+                  img: 'https://picsum.photos/id/353/800/400',
+                },
+              ],
+            },
+            {
+              name: 'slideNavigation3',
+              component: 'ZdRow',
+              justify: 'center',
+              children: [
+                {
+                  name: 'slideCardNavigation3',
+                  component: 'ZdCard',
+                  width: '800',
+                  height: '450',
+                  img: 'https://picsum.photos/id/368/800/400',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    }"
+  />
 </template>
 
 <script>
 export default {
-  name: 'HomeItem'
+  name: "HomeItem",
 };
 </script>
 
